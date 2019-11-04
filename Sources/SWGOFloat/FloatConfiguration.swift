70eb511bc68ec55f4ed73435f49307739a648137
Tomas Martins <tomas.martins@dextra-sw.com>
2019-11-04 12:51:48 -0300

    Created Float Configuration and Requester classes

---
+++ Sources/SWGOFloat/FloatConfiguration.swift
@@ -0,0 +1,71 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tomás Feitoza Martins  on 04/11/19.
+//
+
+import Foundation
+
+public class FloatConfiguration {
+    /// A weapon's inspect link
+    var inspectLink: String?
+    /// Inspect link "s" parameter, if the item is from an player's inventory
+    var inventoryParameter: String?
+    /// Inspect link "a" parameter
+    var aParameter: String?
+    /// Inspect link "d" paramete
+    var dParameter: String?
+    /// Inspect link "m" parameter, if the item is from the Community Market
+    var marketParameter: String?
+    
+    private let baseURL = "https://api.csgofloat.com/"
+    
+    /// Instantiate FloatController with the S, A, D and M parameters
+    public init(inventoryParameter: String?, aParameter: String, dParameter: String, marketParameter: String?) {
+        self.inventoryParameter = inventoryParameter
+        self.aParameter = aParameter
+        self.dParameter = dParameter
+        self.marketParameter = marketParameter
+        self.inspectLink = nil
+    }
+    
+    /// Instantiate FloatController with an item's inpect link
+    init(inspectLink: String) {
+        self.inspectLink = inspectLink
+        self.inventoryParameter = nil
+        self.aParameter = nil
+        self.dParameter = nil
+        self.marketParameter = nil
+    }
+    
+    /**
+     Sets the Request URL depending of which parameters were initiated
+     
+     - Returns: A string representing the URL to be used in the request
+    */
+    public var requestURL: String {
+        if let inspectLink = self.inspectLink {
+            return "\(baseURL)?url=\(inspectLink)"
+        } else if let aParameter = self.aParameter, let dParameter = self.dParameter {
+            var sParameter: String
+            var mParameter: String
+            
+            if let parameter = self.inventoryParameter {
+                sParameter = "s=\(parameter)&"
+            } else {
+                sParameter = ""
+            }
+            
+            if let parameter = self.marketParameter {
+                mParameter = "&m=\(parameter)"
+            } else {
+                mParameter = ""
+            }
+            
+            return "\(baseURL)?\(sParameter)a=\(aParameter)&d=\(dParameter)\(mParameter)"
+        } else {
+            return ""
+        }
+    }
+}
---
+++ Sources/SWGOFloat/FloatRequester.swift
@@ -0,0 +1,69 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tomás Feitoza Martins  on 04/11/19.
+//
+
+import Foundation
+
+public class FloatRequester {
+    public typealias DataReturned = Skin
+    public typealias Completion = ((Skin?, ApiError?) -> Void)
+    public var completion: ((Skin?, ApiError?) -> Void)?
+    public var inspectLink: String
+    
+    public init(configuration: FloatConfiguration, completion: Completion? = nil) {
+        self.completion = completion
+        self.inspectLink = configuration.requestURL
+    }
+    
+    public func start() {
+        guard let requestURL = URL(string: self.inspectLink) else { return }
+        var urlRequest = URLRequest(url: requestURL)
+        urlRequest.httpMethod = "GET"
+        guard let completion = self.completion else { return }
+        
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            guard let data = data else {
+                completion(nil, .unknownError)
+                return
+            }
+            let responseData = String(data: data, encoding: String.Encoding.utf8)
+            dump(responseData)
+            self.parseJson(data: data)
+            if error != nil {
+                completion(nil, .unknownError)
+            }
+        }.resume()
+    }
+    
+    public func parseJson(data: Data) {
+        guard let completion = completion else {
+            return
+        }
+        do {
+            let decodedObject = try self.parseData(data: data)
+            if let errorCode = decodedObject.code {
+                let apiError = ApiError.errorWithCode(code: errorCode)
+                completion(nil, apiError)
+            } else {
+                completion(decodedObject, nil)
+            }
+        }
+        catch {
+            completion(nil, .decodeError)
+        }
+    }
+    
+    public func parseData(data: Data) throws -> Skin{
+        do {
+            let decoder = JSONDecoder()
+            let decodedWebsites = try decoder.decode(Skin.self, from: data)
+            return decodedWebsites
+        } catch {
+            throw ApiError.decodeError
+        }
+    }
+
+}
