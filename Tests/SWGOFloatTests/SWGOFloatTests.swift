0d897d6c7f9c275983e7292ecfa2a1e1295e14fa
Tomas Martins <tomas.martins@dextra-sw.com>
2019-11-04 12:58:27 -0300

    Removed force downcast from unit tests

--- Tests/SWGOFloatTests/SWGOFloatTests.swift
+++ Tests/SWGOFloatTests/SWGOFloatTests.swift
@@ -9,13 +9,13 @@ final class SWGOFloatTests: XCTestCase {
         let config = FloatConfiguration(inspectLink: "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198084749846A698323590D7935523998312483177")
         let request = FloatRequester(configuration: config) { (fetchedSkin, error) in
             XCTAssertNotNil(fetchedSkin)
-            skin = fetchedSkin!
+            guard let actualSkin = fetchedSkin else { return }
+            skin = actualSkin
             exp.fulfill()
         }
         request.start()
         
         waitForExpectations(timeout: 10) { (error) in
-            XCTAssertNotNil(skin)
             XCTAssertNil(skin?.error)
         }
     }
@@ -26,13 +26,14 @@ final class SWGOFloatTests: XCTestCase {
         let config = FloatConfiguration(inventoryParameter: nil, aParameter: "13874827217", dParameter: "4649025583329100061", marketParameter: "2608048286785948758")
         let request = FloatRequester(configuration: config) { (fetchedSkin, error) in
             XCTAssertNotNil(fetchedSkin)
-            skin = fetchedSkin!
+            guard let actualSkin = fetchedSkin else { return }
+            skin = actualSkin
             exp.fulfill()
         }
         request.start()
         
         waitForExpectations(timeout: 10) { (error) in
-            XCTAssertNotNil(skin)
+            XCTAssertNil(skin?.error)
         }
     }
 }
