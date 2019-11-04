ea80c1736dee57416f950db1fc8800031ef83d12
Tomas Martins <tomas.martins@dextra-sw.com>
2019-11-04 12:53:05 -0300

    Created unit tests

--- Tests/SWGOFloatTests/SWGOFloatTests.swift
+++ Tests/SWGOFloatTests/SWGOFloatTests.swift
@@ -4,28 +4,35 @@ import XCTest
 final class SWGOFloatTests: XCTestCase {
     
     func testWithInspectLink() {
-        let config = SWGORequester(inspectLink: "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198084749846A698323590D7935523998312483177")
+        let exp = expectation(description: "InspectLink")
+        var skin: Skin?
+        let config = FloatConfiguration(inspectLink: "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198084749846A698323590D7935523998312483177")
+        let request = FloatRequester(configuration: config) { (fetchedSkin, error) in
+            XCTAssertNotNil(fetchedSkin)
+            skin = fetchedSkin!
+            exp.fulfill()
+        }
+        request.start()
         
-        config.getWeaponInfo { response in
-            switch response {
-            case .success(let skin):
-                XCTAssert(skin.itemInfo?.fullItemName == "Silver Operation Breakout Coin")
-            case .failure( _):
-                XCTFail()
-            }
+        waitForExpectations(timeout: 10) { (error) in
+            XCTAssertNotNil(skin)
+            XCTAssertNil(skin?.error)
         }
     }
     
     func testWithSADMParameters() {
-        let config = SWGORequester(inventoryParameter: nil, aParameter: "13874827217", dParameter: "4649025583329100061", marketParameter: "2608048286785948758")
+        let exp = expectation(description: "Parameters")
+        var skin: Skin?
+        let config = FloatConfiguration(inventoryParameter: nil, aParameter: "13874827217", dParameter: "4649025583329100061", marketParameter: "2608048286785948758")
+        let request = FloatRequester(configuration: config) { (fetchedSkin, error) in
+            XCTAssertNotNil(fetchedSkin)
+            skin = fetchedSkin!
+            exp.fulfill()
+        }
+        request.start()
         
-        config.getWeaponInfo { response in
-            switch response {
-            case .success(let skin):
-                XCTAssert(skin.itemInfo?.name == "Redline")
-            case .failure( _):
-                XCTFail()
-            }
+        waitForExpectations(timeout: 10) { (error) in
+            XCTAssertNotNil(skin)
         }
     }
 }
